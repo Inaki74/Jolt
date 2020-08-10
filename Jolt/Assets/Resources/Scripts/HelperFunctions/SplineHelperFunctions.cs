2c57c4f7d3fb232c60c9ad77e7674a55f4e9ecc0
SleepyWolfy <inaki.2012@hotmail.com>
2020-08-10 01:35:26 -0300

    Setup Rail logic
    Added Spline functions to draw rail, setup to actual rail.

---
+++ Jolt/Assets/Resources/Scripts/HelperFunctions/SplineHelperFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineHelperFunctions
+{
+    public static Vector3 SplineCurve(int n, int i, float t, Transform[] ControlPoints)
+    {
+        if (i > n)
+        {
+            return Vector3.zero;
+        }
+        else
+        {
+            return Comb(n, i) * ControlPoints[i].position * Mathf.Pow(1 - t, n - i) * Mathf.Pow(t, i) + SplineCurve(n, i + 1, t, ControlPoints);
+        }
+    }
+
+    public static int Comb(int n, int i)
+    {
+        return Factorial(n) / (Factorial(n - i) * Factorial(i));
+    }
+
+    public static int Factorial(int n)
+    {
+        if (n <= 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Factorial(n - 1);
+        }
+
+    }
+
+    
+}
---
+++ Jolt/Assets/Resources/Scripts/MovingStuff/RailController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailController : MonoBehaviour
+{
+    [SerializeField] private Transform[] ControlPoints;
+
+    private LineRenderer Lr;
+
+    private List<Vector3> LRPositions = new List<Vector3>();
+
+    private Vector3 gizmosPositions;
+
+    private void Start()
+    {
+        ControlPoints = InitializeControlPoints();
+        Lr = GetComponent<LineRenderer>();
+
+        Lr.startWidth = 0.1f; Lr.endWidth = 0.1f;
+
+        RenderLine();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void RenderLine()
+    {
+        for(float t = 0; t <= 1; t += 0.02f)
+        {
+            LRPositions.Add(SplineHelperFunctions.SplineCurve(ControlPoints.Length - 1, 0, t, ControlPoints));
+        }
+        Lr.positionCount = LRPositions.Count;
+        Lr.SetPositions(LRPositions.ToArray());
+    }
+
+    private Transform[] InitializeControlPoints()
+    {
+        List<Transform> aux = new List<Transform>();
+        foreach(Transform t in transform)
+        {
+            aux.Add(t);
+        }
+        return aux.ToArray();
+    }
+
+    private void OnDrawGizmos()
+    {
+        ControlPoints = InitializeControlPoints();
+
+        for (float t = 0; t <= 1; t += 0.02f)
+        {
+            gizmosPositions = SplineHelperFunctions.SplineCurve(ControlPoints.Length - 1, 0, t, ControlPoints);
+
+            Gizmos.color = Color.red;
+
+            Gizmos.DrawSphere(gizmosPositions, 0.1f);
+        }
+
+    }
+}
