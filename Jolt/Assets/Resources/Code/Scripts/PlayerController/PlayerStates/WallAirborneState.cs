782da99ee6aa5ff31eb20d74ead9fb67fece0bc4
SleepyWolfy <inaki.2012@hotmail.com>
2021-08-04 18:12:55 -0300

    Update WallAirborneState.cs

--- Jolt/Assets/Resources/Code/Scripts/PlayerController/PlayerStates/WallAirborneState.cs
+++ Jolt/Assets/Resources/Code/Scripts/PlayerController/PlayerStates/WallAirborneState.cs
@@ -12,6 +12,8 @@ namespace Jolt
             {
                 protected override Color AssociatedColor => Color.black;
 
+                private float _freefallDeformedScaleX;
+
                 public bool ForceApplied { private get; set; }
 
                 public WallAirborneState(IPlayerStateMachine stateMachine, IPlayer player, IPlayerData playerData) : base(stateMachine, player, playerData)
@@ -21,11 +23,13 @@ namespace Jolt
                 public override void Enter()
                 {
                     base.Enter();
+                    _freefallDeformedScaleX = 1;
                 }
 
                 public override void Exit()
                 {
                     base.Exit();
+                    _player.SetScale(Vector2.one);
                 }
 
                 public override bool LogicUpdate()
@@ -59,11 +63,35 @@ namespace Jolt
                 public override void PhysicsUpdate()
                 {
                     base.PhysicsUpdate();
+
+                    Freefall();
                 }
 
                 protected override void PhysicsFirstStep()
                 {
                     base.PhysicsFirstStep();
+                }
+
+                private void Freefall()
+                {
+                    if (_moveInput.y < 0f)
+                    {
+                        if (_freefallDeformedScaleX > _playerData.MaxDeformedScale)
+                        {
+                            //_freefallDeformedScaleX -= 0.01f;
+                        }
+
+                        Vector2 newScale = new Vector2(_freefallDeformedScaleX, 1f);
+                        _player.SetScale(newScale);
+
+                        _player.SetGravityScale(_playerData.FreeFallGravity);
+                    }
+                    else
+                    {
+                        _freefallDeformedScaleX = 1f;
+                        _player.SetScale(Vector2.one);
+                        _player.SetGravityScale(_playerData.PlayerPhysicsData.StandardGravity);
+                    }
                 }
             }
         }
